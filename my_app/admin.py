43c673fcc64acf3afa91ed96f61d1862ab7bda54
jahidshawon19 <jahidshawon1730@gmail.com>
2022-07-08 09:25:08 +0600

    added database schema

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ProductImage
+from .models import ProductVarients
+from .models import Products
 # Register your models here.
+
+admin.site.register(ProductImage)
+admin.site.register(ProductVarients)
+admin.site.register(Products)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,39 @@
+
 from django.db import models
 
 # Create your models here.
+
+class ProductImage(models.Model):
+    title = models.CharField(max_length=150)
+    photo = models.ImageField(blank=True, upload_to='images/', default='', null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title) 
+
+class ProductVarients(models.Model):
+  
+    color = models.CharField(max_length=150)
+    size = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return str(self.color+"/"+self.size)
+
+
+class Products(models.Model):
+   
+    title = models.CharField(max_length=150)
+    sku = models.CharField(max_length=150)
+    description = models.TextField()
+    varients = models.ManyToManyField(ProductVarients)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    photo = models.ForeignKey(ProductImage, on_delete=models.SET_NULL, null=True, default='') 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title) 
+
